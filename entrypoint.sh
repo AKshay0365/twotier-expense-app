12a7094f723527596ba2a54c036d2e8f8d137a81
AKshay0365 <91820386+AKshay0365@users.noreply.github.com>
2024-01-14 11:46:01 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Wait for the MySQL server to be ready
-# You may need to replace the placeholder command below with an appropriate script/command
-# for waiting for your MySQL server to be ready
-# For example, you can use 'sleep' for a fixed delay or implement a more sophisticated waiting mechanism.
+
 
 
 # Run migrations
